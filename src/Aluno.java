9b12c68d3b3c366be2a010a815ece6576fa61681
Pedro Henrique Lomba <p.henrique.lomba@gmail.com>
2023-08-01 22:23:27 -0300

    1/08

---
+++ src/Aluno.java
@@ -0,0 +1,27 @@
+public class Aluno {
+    private String name;
+    private Integer idade;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public Aluno(String name)
+    {
+        this.setName(name);
+    }
+
+}
